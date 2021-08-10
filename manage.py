618cf75b726da0aec471d674e6ad62d7c52fb78d
Kolynes Chinedu <kolyneschinedu@gmail.com>
2021-08-10 12:25:26 +0100

    renamed

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'recruiter.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Recruiter.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
